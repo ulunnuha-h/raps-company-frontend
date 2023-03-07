c47c8831194ab122fabf4d7901869ae649aba0be
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2023-03-07 15:59:45 +0700

    add social media fetch api

---
+++ src/config/API-url.js
@@ -0,0 +1,7 @@
+const url = 'http://localhost:3001'
+
+const getAPIurl = () => {
+  return url
+}
+
+export default getAPIurl
---
+++ src/config/navLink.js
@@ -0,0 +1,16 @@
+const navLink = [
+  {
+    name: 'Beranda',
+    href: '/'
+  },
+  {
+    name: 'Langkah',
+    href: '/#langkah'
+  },
+  {
+    name: 'Testimoni',
+    href: '/#testimoni'
+  }
+]
+
+export default navLink
