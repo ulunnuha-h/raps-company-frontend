333cd1221a8ce7ab7e43e6d5da7c72c6f2505f3a
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2023-03-11 19:20:20 +0700

    fix poppins font source

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document () {
     <Html lang="en">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
-        <link href="https://fonts.cdnfonts.com/css/poppins" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
