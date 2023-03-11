8cad23daa086307dba39dc26e23721d4e3fc2d4f
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2023-03-11 22:19:37 +0700

    change pembelian success page text

--- src/pages/pembelian/step4.js
+++ src/pages/pembelian/step4.js
@@ -9,8 +9,8 @@ export default function Step4 () {
       <div className='p-7 lg:p-16 bg-[#ACB8DE] flex gap-12 items-center w-fit mx-auto flex-col'>
         <Image src={illustration} alt='illustration' width='350' height='350'></Image>
         <section className='font-grotesk text-primary-900 text-center'>
-          <h3 className='mb-5'>Pembayaran Berhasil!</h3>
-          <span className='text-2xl'>DL/BGL Pembelianmu Akan Segera Menjadi Milikmu!</span>
+          <h3 className='mb-5'>Kurir Meluncurr!!!</h3>
+          <span className='text-2xl'>Ditunggu yaa, dalam 30 detik Diamond Lock kamu akan sampai</span>
         </section>
         <section className='flex justify-around w-full flex-col lg:flex-row gap-5'>
           <button className='btn-primary lg:px-5 py-2'>Buka Aplikasi Growtopia</button>
