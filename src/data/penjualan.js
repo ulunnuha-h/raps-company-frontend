86a7465701c2ff0496a854ff338520dd44f06c57
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2023-03-12 11:13:28 +0700

    double check phone formatter

--- src/data/penjualan.js
+++ src/data/penjualan.js
@@ -1,11 +1,12 @@
 import getAPIurl from '@/config/API-url'
+import phoneNumberFormatter from '@/utilities/phoneNumberFormatter'
 import axios from 'axios'
 
 const postPenjualan = (data) => {
   const form = new FormData()
   form.append('image', data.file)
   form.append('jumlah_dl', data.jumlah)
-  form.append('whatsapp', data.whatsapp)
+  form.append('whatsapp', phoneNumberFormatter(data.whatsapp))
   form.append('transfer', data.metodeBayar)
   form.append('nomor_transfer', data.norekening)
   form.append('nama', data.nama)
